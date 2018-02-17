e68a493bf7a167f6eef99268065af9aed9c169c1
Victor Yurkin <victor.yurkin@gmail.com>
2018-02-16 21:28:20 -0500

    Working on home page.

--- public/js/layout.js
+++ public/js/layout.js
@@ -6,8 +6,10 @@ jQuery(function($){
         function scrollFunction() {
             if (document.body.scrollTop > $('.af-navbar-full').height() || document.documentElement.scrollTop > $('.af-navbar-full').height()) {
                 document.getElementById("navbar").style.top = "0";
+                document.getElementById("af-back-to-top").style.opacity = "0.8";
             } else {
                 document.getElementById("navbar").style.top = "-60px";
+                document.getElementById("af-back-to-top").style.opacity = "0";
             }
         }
     });
